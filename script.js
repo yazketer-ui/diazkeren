e9f8c1b5b6f253d68b259c331cf368768a12bb43
yazketer-ui <yazketer@gmail.com>
2025-11-28 23:45:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
